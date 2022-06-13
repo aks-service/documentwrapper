fd5adff1183dcd6f07018058409ac4e00e80526e
Justin Preuß <jp@aks-service.de>
2022-06-13 13:19:44 +0200

    WIP tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,10 +1,12 @@
 <?php
 
 
+
 it('example', function () {
     expect(true)->toBeTrue();
 });
 
+
 it('is default template a string', function () {
-     expect(\AksService\DocumentWrapper\Document::make()->getTemplate())->toBeString();
+    expect(\AksService\DocumentWrapper\Document::make()->getTemplate())->toBeString();
 });
